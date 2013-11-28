2377fd929e0b34680d09ccaf8a1f9ecfa1c6c940
Sha <shashashasha@gmail.com>
2013-11-27 20:33:06 -0500

    debugging order errors

--- app.js
+++ app.js
@@ -322,15 +322,18 @@ app.post('/ordered', function(req, res) {
     });
   };
 
+  console.log(req.body.line_items[0].properties);
+
   // also keep track of orders in couch
   // not sure if this is smart or dumb
   db_orders.insert(req.body, 'order-' + new Date().getTime(), function (err, body) {
+    console.log(err, body);
 
     var items = req.body.line_items;
     for (var i = 0; i < items.length; i++) {
       var item = items[i],
         docId = item.properties["doc-id"];
-
+        console.log("updating order for docid", docId, item.quantity, item.title);
         updateOrder(docId, item.id, item.quantity, item.title);
     }
   });
