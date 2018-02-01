242bc1016ab24d453c9643b8cd3beaa98c57d099
Sha <shashashasha@gmail.com>
2018-02-01 00:10:17 -0500

    Pulling out shared code

--- processing/process_order.js
+++ processing/process_order.js
@@ -14,6 +14,7 @@ var gm = require('gm')
   , db_orders = nano.db.use('gifpop-orders')
   , Q = require('q')
   , knox = require('knox')
+  , gifpop_utils = require('./process_utils')
   , s3 = knox.createClient({
 	  key: config.AWSCDNAccessKey,
 	  secret: config.AWSCDNSecret,
@@ -37,7 +38,7 @@ var getOrderGifs = function(order_id) {
 			return;
 		}
 
-		if (order.note != '') {
+		if (order.note != null && order.note != '') {
 			console.log("has note:", order.note);
 			return;
 		}
@@ -70,24 +71,7 @@ var getOrderGifs = function(order_id) {
 			} else {
 				console.log(order_id, 'artist gif!');
 
-				var artist_size = 'Artist Large';
-				switch (variant) {
-					case 'Large Square':
-					case '5x5\"':
-						artist_size = 'Large Square';
-						break;
-
-					case 'Small Square':
-					case '3&#189; x 3&#189;\"':
-						artist_size = 'Artist Small';
-						break;
-
-					case '10x10\"':
-					case '10 x 10"':
-					case '10x10"':
-						artist_size = 'Artist Large';
-						break;
-				}
+				var artist_size = gifpop_utils.getArtistSize(variant);
 
 				console.log('product_id', item.product_id, item.title);
 				console.log(artist_info[item.title.toUpperCase()]['print_' + item.product_id]);
@@ -245,7 +229,7 @@ var downloadImages = function(doc) {
 var processFlip = function(doc) {
 	var deferred = Q.defer();
 	var fileroot = doc.order_id + '_' + doc._id,
-		size = getSize(doc.size),
+		size = gifpop_utils.getSize(doc.size),
 		input = 'processing/renamedframes/' + fileroot + '/*',
 		output = 'processing/renamedframes/' + fileroot + '/%03d.jpg',
 		cmd = "convert {input} {rotate} -adaptive-resize {size} -quality 90% {output}",
@@ -255,7 +239,6 @@ var processFlip = function(doc) {
   					.replace("{rotate}", ROTATE ? '-rotate 90' : '')
 						.replace("{size}", "'" + size + "'");
 
-	// console.log('>>>> exporting frames:\t', cmd_exec);
 	exec(cmd_exec, function(err, stdout, stderr) {
 		deferred.resolve(doc);
 	});
@@ -338,6 +321,7 @@ var getTenFrames = function(doc, folder) {
 
 	return cp.join(';');
 };
+
 /*
 	the image processing part, where we get the selected frames
 	and then use gifsicle to chop them out into a new gif
@@ -346,7 +330,7 @@ var chopGif = function(doc) {
 	var deferred = Q.defer();
 
     var fileroot = doc.order_id + '_' + doc._id,
-		size = getSize(doc.size),
+		size = gifpop_utils.getSize(doc.size),
 		mkdir = "mkdir processing/frames/{folder}; mkdir processing/renamedframes/{folder};".replace("{folder}", fileroot).replace("{folder}", fileroot),
 		// if we want a custom background use hex like -background '#f1f7f7'
 		// this crops from one side of the image: "-gravity East -chop 100x0"
@@ -391,7 +375,7 @@ var zipGifChop = function(doc) {
 		tempframes = './processing/renamedframes/',
 		fileroot = doc.order_id + '_' + doc._id,
 		filename = tempchoppt + getFilename(doc, 'gif'),
-		size = getSize(doc.size);
+		size = gifpop_utils.getSize(doc.size);
 
 	// in case we want to change the frames in the admin, we don't assume ranges here
   var frames = doc.frames.split(','),
@@ -466,136 +450,6 @@ var getCurrentUploadFolder = function() {
     return 'zips/' + [d.getFullYear(), month, date].join('-') + '/';
 };
 
-var getSize = function(size) {
-	var words = size.split(' '),
-		size = words.length > 2 ? words[0] + ' ' + words[1] : size;
-
-	var realwords = [];
-	words.forEach(function(word, i) {
-		if (word.charAt(word.length-1) != '+') {
-			realwords.push(word);
-		}
-	});
-
-	var realsize = realwords.join(' ');
-
-	switch (realsize) {
-		case 'Business Card':
-			// return "4860x3060^"; // 1440 dpi
-			return "1012x637^";
-		case 'Postcard':
-		case 'Landscape Postcard':
-			// return "7200x5040^"; // 1440 dpi
-			return "1500x1050^";
-
-		case 'Portrait Postcard':
-			// return "5040x7200^"; // 1440 dpi
-			return "1050x1500^";
-
-		case 'Artist Print':
-		case 'Artist Large':
-			return "3000x3000^";
-
-		case 'Large Square':
-			// return "3000x3000^"; // 600 dpi
-			// return "7200x7200^"; // 1440 dpi
-			return "1500x1500^";
-
-		case 'Small Square':
-			// return "3960x3960^"; // 1440 dpi
-			return "825x825^";
-
-		default:
-			console.log("unable to find", size);
-			// return "825x825^";
-			return "700x700^";
-	}
-};
-
-// manufacturer's internal product ids
-// 2.75 Instagram Motion Print	CAP01A0A
-// 3.5 Instagram Motion Print	CAQ01A0A
-// Large Motion Print			AA20110A
-// Wallet Motion Print			AA10110A
-// 5” x 5”						AAS01A00
-// 10” x 10”					AAT01A00
-var getProductId = function(size) {
-	var words = size.split(' '),
-		realsize = [];
-
-	words.forEach(function(word, i) {
-		if (word.charAt(word.length-1) != '+') {
-			realsize.push(word);
-		}
-	});
-
-	realsize = realsize.join(' ');
-
-	switch (realsize) {
-		case 'Business Card':
-			return "AA10110A3";
-		case 'Portrait Business Card':
-			return "AA10110A31";
-
-		case 'Postcard ':
-		case 'Postcard':
-		case 'Landscape Postcard':
-			return "AA20110A3";
-
-		case 'Portrait Postcard':
-			return "AA20110A31";
-
-		case 'Large Square':
-		case '5x5\"':
-			return "AAS01A003";
-
-		case 'Artist Small':
-		case '3&#189; x 3&#189;\"':
-			return "CAQ01A0A3";
-
-		case 'Artist Large':
-		case 'Artist Print':
-		case '10 x 10\"':
-		case '10 x 10"':
-			return "AAT01A003"
-
-		case 'Small Square':
-			return "CAP01A0A3";
-
-		case 'Small Sticker':
-			return "STKQ00";
-
-		default:
-			console.log("unable to find", size);
-			return "unknown";
-	}
-};
-
-var getShippingMethod = function(order) {
-	var method = 'standard';
-
-	if (order.shipping_lines && order.shipping_lines[0] && order.shipping_lines[0].code) {
-		var code = order.shipping_lines[0].code;
-		switch (code.toLowerCase()) {
-			case '2day':
-			case '2 day':
-			case 'fedex 2-day':
-			case 'fedex 2 day':
-			case 'fedex 2-day international':
-			case "fedex international shipping, 2-3 days":
-				return '2day';
-			case 'priority':
-			case 'usps priority':
-			case 'usps priority — with tracking number':
-				return 'priority';
-			case 'overnight':
-				return 'overnight';
-		}
-	}
-
-	return method;
-};
-
 var makeFullOrderRequest = function(order_details) {
 	if (!order_details.gifs) {
 		console.log(order_details.id, 'ended before making request');
@@ -650,7 +504,7 @@ var makeFullOrderRequest = function(order_details) {
 
 		var item = {
 			lineId: gif.id + gif.size.toLowerCase().split(' ').join('') + i.toString(),
-			productId: getProductId(gif.size),
+			productId: gifpop_utils.getProductId(gif.size),
 			productInfo: gif.size,
 			productName: gif.product_name,
 			quantity: gif.quantity,
@@ -690,7 +544,7 @@ var makeFullOrderRequest = function(order_details) {
 		shippingPostalCode: shipping.zip,
 		shippingCountry:  	shipping.country_code,
 		shippingPhone: 		shipping.phone,
-		shippingMethod: 	getShippingMethod(order)
+		shippingMethod: 	gifpop_utils.getShippingMethod(order)
 	};
 
 	console.log(full_order);
@@ -715,8 +569,6 @@ var makeFullOrderRequest = function(order_details) {
 				fs.appendFile('processing/render_logs.txt', csv.join(','), function (err) { });
 	    	}
 			console.log(err, body);
-
-			// db_orders.get()
 		});
 	}
 };
@@ -802,7 +654,7 @@ process.argv.forEach(function (val, index, array) {
 	} else if (val.search("borderhex=") == 0) {
 		BORDER_HEX = val.split("borderhex=")[1];
 		console.log('setting border hex color to', BORDER_HEX);
-	} else if (val.search("line_item_range=") == 0 || val.search("line_id_range=") == 0) {
+	} else if (val.search("line_ids=") == 0 || val.search("line_item_range=") == 0 || val.search("line_id_range=") == 0) {
 		var range = val.split('=')[1].split('-');
 		LINE_ITEM_START = +range[0];
 		LINE_ITEM_END = +range[1];
---
+++ processing/process_utils.js
@@ -0,0 +1,161 @@
+// process_utils.js
+// helper functions for interacting with orders
+
+// Given a card size, return the pixel size
+var getSize = function(size) {
+  var words = size.split(' '),
+    size = words.length > 2 ? words[0] + ' ' + words[1] : size;
+
+  var realwords = [];
+  words.forEach(function(word, i) {
+    if (word.charAt(word.length-1) != '+') {
+      realwords.push(word);
+    }
+  });
+
+  var realsize = realwords.join(' ');
+
+  switch (realsize) {
+    case 'Business Card':
+      // return "4860x3060^"; // 1440 dpi
+      return "1012x637^";
+    case 'Postcard':
+    case 'Landscape Postcard':
+      // return "7200x5040^"; // 1440 dpi
+      return "1500x1050^";
+
+    case 'Portrait Postcard':
+      // return "5040x7200^"; // 1440 dpi
+      return "1050x1500^";
+
+    case 'Artist Print':
+    case 'Artist Large':
+      return "3000x3000^";
+
+    case 'Large Square':
+      // return "3000x3000^"; // 600 dpi
+      // return "7200x7200^"; // 1440 dpi
+      return "1500x1500^";
+
+    case 'Small Square':
+      // return "3960x3960^"; // 1440 dpi
+      return "825x825^";
+
+    default:
+      console.log("unable to find", size);
+      // return "825x825^";
+      return "700x700^";
+  }
+};
+
+var getArtistSize = function(variant) {
+  switch (variant) {
+    case 'Large Square':
+    case '5x5\"':
+      return 'Large Square';
+
+    case 'Small Square':
+    case '3&#189; x 3&#189;\"':
+      return 'Artist Small';
+
+    case '10x10\"':
+    case '10 x 10"':
+    case '10x10"':
+      return 'Artist Large';
+
+    default:
+      return 'Artist Large';
+  }
+};
+
+// Manufacturer's product codes
+// 2.75 Instagram Motion Print  CAP01A0A
+// 3.5 Instagram Motion Print CAQ01A0A
+// Large Motion Print     AA20110A
+// Wallet Motion Print      AA10110A
+// 5” x 5”            AAS01A00
+// 10” x 10”          AAT01A00
+var getProductId = function(size) {
+  var words = size.split(' '),
+    realsize = [];
+
+  words.forEach(function(word, i) {
+    if (word.charAt(word.length-1) != '+') {
+      realsize.push(word);
+    }
+  });
+
+  realsize = realsize.join(' ');
+
+  switch (realsize) {
+    case 'Business Card':
+      return "AA10110A3";
+    case 'Portrait Business Card':
+      return "AA10110A31";
+
+    case 'Postcard ':
+    case 'Postcard':
+    case 'Landscape Postcard':
+      return "AA20110A3";
+
+    case 'Portrait Postcard':
+      return "AA20110A31";
+
+    case 'Large Square':
+    case '5x5\"':
+      return "AAS01A003";
+
+    case 'Artist Small':
+    case '3&#189; x 3&#189;\"':
+      return "CAQ01A0A3";
+
+    case 'Artist Large':
+    case 'Artist Print':
+    case '10 x 10\"':
+    case '10 x 10"':
+      return "AAT01A003"
+
+    case 'Small Square':
+      return "CAP01A0A3";
+
+    case 'Small Sticker':
+      return "STKQ00";
+
+    default:
+      console.log("unable to find", size);
+      return "unknown";
+  }
+};
+
+
+// Manufacturer's shipping codes
+var getShippingMethod = function(order) {
+  var method = 'standard';
+
+  if (order.shipping_lines && order.shipping_lines[0] && order.shipping_lines[0].code) {
+    var code = order.shipping_lines[0].code;
+    switch (code.toLowerCase()) {
+      case '2day':
+      case '2 day':
+      case 'fedex 2-day':
+      case 'fedex 2 day':
+      case 'fedex 2-day international':
+      case "fedex international shipping, 2-3 days":
+        return '2day';
+      case 'priority':
+      case 'usps priority':
+      case 'usps priority — with tracking number':
+        return 'priority';
+      case 'overnight':
+        return 'overnight';
+    }
+  }
+
+  return method;
+};
+
+module.exports = {
+  getSize: getSize,
+  getProductId: getProductId,
+  getShippingMethod: getShippingMethod
+};
